5511a127b1a1803590897efd17961e646f9ded08
Carlos Pereira <carloscoto0103@gmail.com>
2024-02-21 20:29:04 -0600

    Se cambia el nombre de mensaje de error

--- src/compiladores/ManejoErrores.java
+++ src/compiladores/ManejoErrores.java
@@ -19,7 +19,7 @@ public class ManejoErrores {
         errores.put(2, "01. Error: Falta el componente Module Program o esta mal escrito.");
         errores.put(3, "02. Error: Los imports deben estar antes del Module Program");
         errores.put(4, "03. Error: Después del End Module no deberían haber comandos a excepción de los comentarios.");
-        errores.put(5, "04. Error: El camando Sub Main no concluye en la misma linea.");
+        errores.put(5, "04. Error: El comando Sub Main no concluye en la misma linea.");
         errores.put(6, "05. Error: Falta el componente Sub Main o esta mal escrito.");
         errores.put(7, "06. Error: Falta el componente End Sub o esta mal escrito.");
         errores.put(8, "07. Error: La linea supera los 90 caracteres.");
